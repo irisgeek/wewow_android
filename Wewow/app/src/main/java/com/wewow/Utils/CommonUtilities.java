d2ea8d7f649a1de4d32776c00d10eeabb97cab6e
suncjs <suncjs@yeah.net>
2017-03-19 01:35:04 +0800

    add wx info; move app reg info into codes

--- Wewow/app/src/main/java/com/wewow/Utils/CommonUtilities.java
+++ Wewow/app/src/main/java/com/wewow/Utils/CommonUtilities.java
@@ -10,4 +10,11 @@ public class CommonUtilities {
     public static final String SUCCESS="success";
 
 
+    public static final String Weibo_AppKey = "3768596648";
+    public static final String Weibo_AppSecret = "0225c1f9f4b1a9858f64d9a7e865ef23";
+    public static final String Weibo_Redirect_URL = "https://api.weibo.com/oauth2/default.html";
+    public static final String Weibo_Scope = "";
+
+    public static final String WX_AppID = "wx30a31e723d2983a2";
+    public static final String WX_AppKey = "2eb4be156bb3d655489c3cf5d93e0187";
 }
