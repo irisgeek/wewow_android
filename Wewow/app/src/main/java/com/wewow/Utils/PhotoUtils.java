b3c190636ac82416d3bdf0e19cacb662de042851
suncjs <suncjs@yeah.net>
2017-04-10 22:11:03 +0800

    add picture methods

--- Wewow/app/src/main/java/com/wewow/Utils/PhotoUtils.java
+++ Wewow/app/src/main/java/com/wewow/Utils/PhotoUtils.java
@@ -1,7 +1,14 @@
 package com.wewow.utils;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,10 +25,9 @@ public class PhotoUtils {
 
 
     /**
-     *
      * @return
      */
-    public static Uri createImageFile(){
+    public static Uri createImageFile() {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
@@ -42,7 +48,8 @@ public class PhotoUtils {
         // Save a file: path for use with ACTION_VIEW intents
         return Uri.fromFile(image);
     }
-    public static void copyFileUsingFileChannels(File source, File dest){
+
+    public static void copyFileUsingFileChannels(File source, File dest) {
         FileChannel inputChannel = null;
         FileChannel outputChannel = null;
         try {
@@ -64,4 +71,39 @@ public class PhotoUtils {
             }
         }
     }
+
+
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if (bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    public static RoundedBitmapDrawable createRoundedDrawable(Context context, Bitmap bm, float cornerRadius) {
+        Bitmap dst = bm.getWidth() > bm.getHeight() ?
+                Bitmap.createBitmap(bm, bm.getWidth() / 2, bm.getHeight() / 2, bm.getHeight() / 2, bm.getHeight() / 2) :
+                Bitmap.createBitmap(bm, bm.getWidth() / 2, bm.getHeight() / 2, bm.getWidth() / 2, bm.getWidth() / 2);
+        float radius = bm.getWidth() / 2 > cornerRadius ? cornerRadius : bm.getWidth() / 2;
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(context.getResources(), dst);
+        roundedBitmapDrawable.setCornerRadius(radius);
+        roundedBitmapDrawable.setAntiAlias(true);
+        return roundedBitmapDrawable;
+    }
+
 }
