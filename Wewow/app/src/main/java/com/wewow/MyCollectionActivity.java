b3914edd20e010e8758d654394b9bfd3e34f7d0f
suncjs <suncjs@yeah.net>
2017-05-04 23:39:25 +0800

    draft my collection

--- Wewow/app/src/main/java/com/wewow/MyCollectionActivity.java
+++ Wewow/app/src/main/java/com/wewow/MyCollectionActivity.java
@@ -1,6 +1,8 @@
 package com.wewow;
 
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +10,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.util.Pair;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.wewow.utils.CommonUtilities;
 import com.wewow.utils.HttpAsyncTask;
+import com.wewow.utils.RemoteImageLoader;
 import com.wewow.utils.Utils;
 import com.wewow.utils.WebAPIHelper;
 
@@ -29,6 +38,8 @@ public class MyCollectionActivity extends BaseActivity {
     private static final String TAG = "MyCollectionActivity";
     private LinearLayout labs_container;
     private JSONObject likedinfo;
+    private ListView mycollist;
+    private JSONArray articles = new JSONArray();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +54,58 @@ public class MyCollectionActivity extends BaseActivity {
 
     private void setupUI() {
         this.labs_container = (LinearLayout) this.findViewById(R.id.collected_lablist);
+        this.mycollist = (ListView) this.findViewById(R.id.mycollection_list);
+        this.mycollist.setAdapter(this.listAdpater);
+        this.mycollist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                int id = (Integer)view.getTag();
+                Intent ai = new Intent(MyCollectionActivity.this, ArticleActivity.class);
+                ai.putExtra(ArticleActivity.ARTICLE_ID, id);
+                MyCollectionActivity.this.startActivity(ai);
+            }
+        });
     }
+
+    private BaseAdapter listAdpater = new BaseAdapter() {
+        @Override
+        public int getCount() {
+            return MyCollectionActivity.this.articles.length();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return MyCollectionActivity.this.articles.opt(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                view = View.inflate(MyCollectionActivity.this, R.layout.mycollection_article_item, null);
+            }
+            JSONObject jobj = (JSONObject) this.getItem(i);
+            final ImageView iv = (ImageView) view.findViewById(R.id.mycollection_pic);
+            new RemoteImageLoader(MyCollectionActivity.this, jobj.optString("image_642_320"), new RemoteImageLoader.RemoteImageListener() {
+                @Override
+                public void onRemoteImageAcquired(Drawable dr) {
+                    BitmapDrawable bdr = (BitmapDrawable) iv.getDrawable();
+                    if (bdr != null) {
+                        bdr.getBitmap().recycle();
+                    }
+                    iv.setImageDrawable(dr);
+                }
+            });
+            TextView tv = (TextView) view.findViewById(R.id.mycollection_title);
+            tv.setText(jobj.optString("title"));
+            view.setTag(jobj.optInt("id"));
+            return view;
+        }
+    };
 
     private void loadData() {
         if (!UserInfo.isUserLogged(this)) {
@@ -78,7 +140,7 @@ public class MyCollectionActivity extends BaseActivity {
         this.labs_container.removeAllViews();
         this.likedinfo = jobj;
         try {
-            JSONArray articles = jobj.getJSONArray("articles");
+            this.articles = jobj.getJSONArray("articles");
             JSONArray collections = jobj.getJSONArray("collections");
             for (int i = 0; i < collections.length(); i++) {
                 JSONObject collection = collections.getJSONObject(i);
@@ -86,7 +148,14 @@ public class MyCollectionActivity extends BaseActivity {
                 col.setText(collection.optString("collection_title"));
                 col.setTag(collection.optString("collection_id"));
                 this.labs_container.addView(col);
+                LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) col.getLayoutParams();
+                lp.gravity = Gravity.CENTER;
+                lp.width = Utils.dipToPixel(this, 75);
+                lp.height = LinearLayout.LayoutParams.MATCH_PARENT;
+                lp.setMargins(Utils.dipToPixel(this, 4), 0, 0, 0);
+                col.setLayoutParams(lp);
             }
+            this.listAdpater.notifyDataSetChanged();
         } catch (JSONException e) {
             Log.e(TAG, String.format("get collection list fail: %s", e.getMessage()));
             Toast.makeText(MyCollectionActivity.this, R.string.serverError, Toast.LENGTH_LONG).show();
